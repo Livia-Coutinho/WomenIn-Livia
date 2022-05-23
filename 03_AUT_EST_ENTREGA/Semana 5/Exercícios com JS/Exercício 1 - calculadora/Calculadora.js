56e5005af3d2d6624eb7af6234caa8bb62a15492
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-05-23 11:37:44 -0300

    Calculadora (HMTL e JS)

---
+++ 03_AUT_EST_ENTREGA/Semana 5/Exercícios com JS/Exercício 1 - calculadora/Calculadora.js
@@ -0,0 +1,45 @@
+var numero1
+var numero2
+var resultado
+
+function adição() {
+    numero1 = document.getElementById("numero1").value 
+    numero2 = document.getElementById("numero2").value
+    resultado = Number(numero1) + Number("numero2")
+    document.getElementById("resultado").value = resultado
+}
+
+function subtração() {
+    numero1 = document.getElementById("numero1").value 
+    numero2 = document.getElementById("numero2").value
+    resultado = Number(numero1) - Number("numero2")
+    document.getElementById("resultado").value = resultado
+}
+
+function multiplicação() {
+    numero1 = document.getElementById("numero1").value 
+    numero2 = document.getElementById("numero2").value
+    resultado = number(numero1) * number(numero2)
+    document.getElementById("resultado").value = resultado
+}
+
+function divisão() {
+    numero1 = document.getElementById("numero1").value 
+    numero2 = document.getElementById("numero2").value
+    resultado = number(numero1) / number(numero2)
+    document.getElementById("resultado").value = resultado
+}
+
+function divisãoInt() {
+    numero1 = document.getElementById("numero1").value 
+    numero2 = document.getElementById("numero2").value
+    resultado = number(numero1) / number(numero2)
+    document.getElementById("resultado").value = parseInt(resultado)
+}
+
+function divisãoResto() {
+    numero1 = document.getElementById("numero1").value 
+    numero2 = document.getElementById("numero2").value
+    resultado = number(numero1) % number(numero2)
+    document.getElementById("resultado").value = resultado
+}s
