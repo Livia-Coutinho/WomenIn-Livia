f82fe8c8f9944a5332c966c1c334ad66cf03b6e3
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-06-03 21:59:35 -0300

    Currículo

---
+++ 03_AUT_EST_ENTREGA/Semana 7/Currículo - Semana 7/index.js
@@ -0,0 +1,6 @@
+window.alert("Olá, seja bem vindo ao meu currículo.") //Janela com botão Ok
+window.prompt ("Por favor, antes de acessar o currículo, informe-me o seu nome.") //Irá perguntar o nome
+
+$( document ).ready(function() {
+    alert("J.query")
+})
