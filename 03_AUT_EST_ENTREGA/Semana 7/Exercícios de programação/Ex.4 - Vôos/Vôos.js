49e4d70940a226396d8908afeb83c8ca533abb9d
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-06-07 14:33:02 -0300

    Alterações

---
+++ 03_AUT_EST_ENTREGA/Semana 7/Exercícios de programação/Ex.4 - Vôos/Vôos.js
@@ -0,0 +1,30 @@
+function calculus() {
+  var periodo = document.getElementByName("turno");
+  var total;
+
+  if (periodo[0].checked) {
+    var nPassageiros = parseInt(document.querySelector("#passageiros").value);
+    if (nPassageiros > 50) {
+      total = numPessoas * 120;
+      document.querySelector("#calcular").innerHTML =
+        "O valor total é R$${total}.";
+    } else {
+      total = nPassageiros * 200;
+      document.querySelector("#calcular").innerHTML =
+        "O valor total é R$${total}";
+    }
+  } 
+  
+    else if (periodo[1].checked) {
+    var nPassageiros = parseInt(document.querySelector("#passageiros").value);
+    if (nPassageiros > 50) {
+      total = nPassageiros * 80;
+      document.querySelector("#calcular").innerHTML =
+        "O valor total é R$${total}";
+    } else {
+      total = nPassageiros * 100;
+      document.querySelector("#calcular").innerHTML =
+        "O valor total é R$${total}";
+    }
+  }
+}
