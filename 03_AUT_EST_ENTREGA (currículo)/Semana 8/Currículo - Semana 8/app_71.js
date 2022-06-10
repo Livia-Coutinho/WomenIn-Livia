588d7572b4542595f06f609a1f295dfcd77bbfee
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-06-10 10:05:54 -0300

    Semana 8 - DB

--- 03_AUT_EST_ENTREGA (currículo)/Semana 8/Currículo - Semana 8/app_71.js
+++ 03_AUT_EST_ENTREGA (currículo)/Semana 8/Currículo - Semana 8/app_71.js
@@ -19,6 +19,52 @@ app.use(express.json());
 /* Definição dos endpoints */
 
 /****** CRUD ******************************************************************/
+app.get('/teste', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT id,lingua,nivel FROM idiomas ORDER BY id COLLATE NOCASE';
+    db.all(sql, [],  (err, rows ) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+    db.close(); // Fecha o banco
+});
+
+/* Definição dos endpoints */
+app.get('/', (req, res) => {
+    var resultado = "";
+    //var ordem = 'title';
+    res.statusCode = 200;
+    //console.log("aqui" + req.query.order);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //if(req.query.order) ordem = req.query.order;
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT id,lingua,nivel FROM idiomas ORDER BY id COLLATE NOCASE';
+    console.log(sql);
+    db.serialize(function () {
+      db.each(sql,
+        function (erruserId, row) {
+          //console.log(row. + ': ' + row.title);
+          resultado += '<div class="linha">' + row.id + '</span> | ' + row.lingua + '</div>\n';
+          console.log(resultado);
+        });
+    });
+    
+    res.write('<!DOCTYPE html> \n<meta charset="UTF-8">\n<head> \n\t<title>Etapa 0 - LISTAGEM DE USUÁRIOS</title><style>.linha { border-bottom: solid 1px black;}</style>\n</head> \
+  \n<body> \
+   \n\t<div id="main"> \
+       \n\t\t<h1> MEU CURRÍCULO </h1>');
+    res.write('\n\t\t<div class="linha">' + resultado + '</div> \n\t</div>');
+    res.write(resultado);
+    res.write('\n</body> \n</html>');
+    res.end();
+    resultado = "";
+  });
+
 
 // Retorna todos registros (é o R do CRUD - Read)
 app.get('/users', (req, res) => {
